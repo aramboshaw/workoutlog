9ed33461757acbf78d0dd552426d967e9708c015
Ashley Shaw <a.ramboshaw@gmail.com>
2021-06-17 16:28:35 -0400

    cleaned up code

--- server/controllers/usercontroller.js
+++ server/controllers/usercontroller.js
@@ -36,23 +36,6 @@ router.post("/register", async (req, res) => {
     }
 });
 
-
-//     let { email, password } = req.body.user;
-// try {
-// const User = await UserModel.create({
-//         email,
-//         password:bcrypt.hashSync(password,13),
-//     });
-
-// let token = jwt.sign({id: User.id}, process.env.JWT_SECRET, {expiresIn: 60*60*24});
-
-//     res.status(201).json({
-//         message: "User successfully registered",
-//         user: User,
-//         sessionToken: token
-//     });
-
-
 router.post("/login", async(req, res) => {
     let { username, password } = req.body.user;
     try {
@@ -91,37 +74,7 @@ router.post("/login", async(req, res) => {
         }
         });
 
-//     if (loginUser) {
 
-//         let passwordComparison = await bcrypt.compare(password, loginUser.password);
-
-//         if (passwordComparison) {
-
-//         let token = jwt.sign({id: loginUser.id}, process.env.JWT_SECRET, {expiresIn: 60*60*24});
-
-//     res.status(200).json({
-//         user: loginUser,
-//         message: "User successfully logged in.",
-//         sessionToken: token
-//     });
-
-// }else {
-//     res.status(401).json({
-//         message: "Incorrect email or password"
-// })
-// }
-
-//     } else { 
-//         res.status(401).json({
-//             message: "Incorrect email or password"
-//         });
-//     }
-// } catch (error) {
-//     res.status(500).json({
-//         message: "Failed to log user in."
-//     })
-// }
-// });
 
 
 module.exports = router;
